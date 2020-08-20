7681ca66f48a3ed0b4d246bec89dd55e8a5ce847
Ivar Rydstrom <45040436+BlizardWizard@users.noreply.github.com>
2020-08-19 23:01:38 -0700

    changed homepage renderer width to ratio of screen width

--- personal-youtube-modifications.js
+++ personal-youtube-modifications.js
@@ -37,10 +37,12 @@ function fixHomepage() {
         });
     };
 
-    // set videos per row to 6, width of video grid to 73%, app drawer width to 10.5%, re-center grid on page
-    document.querySelector('ytd-rich-grid-renderer').setAttribute('style', '--ytd-rich-grid-items-per-row: 6; width: 73%; margin: auto');
-    document.querySelector('ytd-app').style.setProperty('--app-drawer-width', '10.5%');
-    document.querySelector('ytd-page-manager').style.setProperty('margin-left', '10.5%');
+    // set videos per row to 6, width of video grid to 73% screen width, app drawer width to 10.5%, re-center grid on page
+    var inputRenderWidth = 1400/1920*window.innerWidth;
+    var inputDrawerWidth = 200/1920*innerWidth;
+    document.querySelector('ytd-rich-grid-renderer').setAttribute('style', `--ytd-rich-grid-items-per-row: 6; width: ${inputRenderWidth}px; margin: auto`);
+    document.querySelector('ytd-app').style.setProperty('--app-drawer-width', `${inputDrawerWidth}px`);
+    document.querySelector('ytd-page-manager').style.setProperty('margin-left', `${inputDrawerWidth}px`);
 
     // make video title font size, etc smaller
     document.querySelectorAll('#video-title.ytd-rich-grid-video-renderer').forEach(function(title) {
